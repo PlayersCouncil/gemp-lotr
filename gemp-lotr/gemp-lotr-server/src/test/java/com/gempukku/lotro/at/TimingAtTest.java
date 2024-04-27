715736321471d9a1b8fb5a4e445d11bf8e27228e
MarcinSc <marcins78@gmail.com>
2024-04-27 22:29:09 +0700

    Migrated Gollum cards in set 7 to hjson

--- gemp-lotr/gemp-lotr-server/src/test/java/com/gempukku/lotro/at/TimingAtTest.java
+++ gemp-lotr/gemp-lotr-server/src/test/java/com/gempukku/lotro/at/TimingAtTest.java
@@ -96,9 +96,9 @@ public class TimingAtTest extends AbstractAtTest {
         Map<String, Collection<String>> extraCards = new HashMap<>();
         initializeSimplestGame(extraCards);
 
-        PhysicalCardImpl elrond = new PhysicalCardImpl(100, "1_40", P1, _cardLibrary.getLotroCardBlueprint("1_40"));
-        PhysicalCardImpl gimli = new PhysicalCardImpl(101, "1_13", P1, _cardLibrary.getLotroCardBlueprint("1_13"));
-        PhysicalCardImpl dwarvenHeart = new PhysicalCardImpl(102, "1_10", P1, _cardLibrary.getLotroCardBlueprint("1_10"));
+        PhysicalCard elrond = _game.getGameState().createPhysicalCard(P1, _cardLibrary, "1_40");
+        PhysicalCard gimli = _game.getGameState().createPhysicalCard(P1, _cardLibrary, "1_13");
+        PhysicalCard dwarvenHeart = _game.getGameState().createPhysicalCard(P1, _cardLibrary, "1_10");
 
         _game.getGameState().addCardToZone(_game, gimli, Zone.FREE_CHARACTERS);
         _game.getGameState().addCardToZone(_game, elrond, Zone.SUPPORT);
@@ -119,8 +119,8 @@ public class TimingAtTest extends AbstractAtTest {
         Map<String, Collection<String>> extraCards = new HashMap<>();
         initializeSimplestGame(extraCards);
 
-        PhysicalCardImpl aragorn = new PhysicalCardImpl(100, "1_365", P1, _cardLibrary.getLotroCardBlueprint("1_365"));
-        PhysicalCardImpl gandalf = new PhysicalCardImpl(101, "2_122", P1, _cardLibrary.getLotroCardBlueprint("2_122"));
+        PhysicalCard aragorn = _game.getGameState().createPhysicalCard(P1, _cardLibrary, "1_365");
+        PhysicalCard gandalf = _game.getGameState().createPhysicalCard(P1, _cardLibrary, "2_122");
 
         _game.getGameState().addCardToZone(_game, aragorn, Zone.FREE_CHARACTERS);
         _game.getGameState().addCardToZone(_game, gandalf, Zone.FREE_CHARACTERS);
@@ -147,7 +147,7 @@ public class TimingAtTest extends AbstractAtTest {
         Map<String, Collection<String>> extraCards = new HashMap<>();
         initializeSimplestGame(extraCards);
 
-        PhysicalCardImpl gollum = new PhysicalCardImpl(100, "7_58", P2, _cardLibrary.getLotroCardBlueprint("7_58"));
+        PhysicalCard gollum = _game.getGameState().createPhysicalCard(P2, _cardLibrary, "7_58");
 
         _game.getGameState().addCardToZone(_game, gollum, Zone.DISCARD);
 
@@ -208,9 +208,9 @@ public class TimingAtTest extends AbstractAtTest {
         Map<String, Collection<String>> extraCards = new HashMap<>();
         initializeSimplestGame(extraCards);
 
-        PhysicalCardImpl gimli = new PhysicalCardImpl(100, "1_13", P1, _cardLibrary.getLotroCardBlueprint("1_13"));
-        PhysicalCardImpl letThemCome = new PhysicalCardImpl(101, "1_20", P1, _cardLibrary.getLotroCardBlueprint("1_20"));
-        PhysicalCardImpl tossMe = new PhysicalCardImpl(102, "6_11", P1, _cardLibrary.getLotroCardBlueprint("6_11"));
+        PhysicalCard gimli = _game.getGameState().createPhysicalCard(P1, _cardLibrary, "1_13");
+        PhysicalCard letThemCome = _game.getGameState().createPhysicalCard(P1, _cardLibrary, "1_20");
+        PhysicalCard tossMe = _game.getGameState().createPhysicalCard(P1, _cardLibrary, "6_11");
 
         skipMulligans();
 
@@ -342,11 +342,11 @@ public class TimingAtTest extends AbstractAtTest {
 
         skipMulligans();
 
-        PhysicalCardImpl balinAvenged = new PhysicalCardImpl(100, "17_2", P1, _cardLibrary.getLotroCardBlueprint("17_2"));
-        PhysicalCardImpl prowlingOrc = new PhysicalCardImpl(101, "11_136", P2, _cardLibrary.getLotroCardBlueprint("11_136"));
-        PhysicalCardImpl dwarvenGuard1 = new PhysicalCardImpl(102, "1_7", P1, _cardLibrary.getLotroCardBlueprint("1_7"));
-        PhysicalCardImpl dwarvenGuard2 = new PhysicalCardImpl(103, "1_7", P1, _cardLibrary.getLotroCardBlueprint("1_7"));
-        PhysicalCardImpl prowlingOrcInDiscard = new PhysicalCardImpl(104, "11_136", P2, _cardLibrary.getLotroCardBlueprint("11_136"));
+        PhysicalCard balinAvenged = _game.getGameState().createPhysicalCard(P1, _cardLibrary, "17_2");
+        PhysicalCard prowlingOrc = _game.getGameState().createPhysicalCard(P2, _cardLibrary, "11_136");
+        PhysicalCard dwarvenGuard1 = _game.getGameState().createPhysicalCard(P1, _cardLibrary, "1_7");
+        PhysicalCard dwarvenGuard2 = _game.getGameState().createPhysicalCard(P1, _cardLibrary, "1_7");
+        PhysicalCard prowlingOrcInDiscard = _game.getGameState().createPhysicalCard(P2, _cardLibrary, "11_136");
 
         _game.getGameState().addCardToZone(_game, balinAvenged, Zone.SUPPORT);
         _game.getGameState().addTokens(balinAvenged, Token.DWARVEN, 4);
@@ -382,10 +382,10 @@ public class TimingAtTest extends AbstractAtTest {
 
         skipMulligans();
 
-        PhysicalCardImpl gimlisHelm = new PhysicalCardImpl(100, "1_15", P1, _cardLibrary.getLotroCardBlueprint("1_15"));
+        PhysicalCard gimlisHelm = _game.getGameState().createPhysicalCard(P1, _cardLibrary, "1_15");
         _game.getGameState().attachCard(_game, gimlisHelm, _game.getGameState().getRingBearer(P1));
 
-        PhysicalCardImpl urukHaiRaidingParty = new PhysicalCardImpl(103, "1_158", P2, _cardLibrary.getLotroCardBlueprint("1_158"));
+        PhysicalCard urukHaiRaidingParty = _game.getGameState().createPhysicalCard(P2, _cardLibrary, "1_158");
         _game.getGameState().addCardToZone(_game, urukHaiRaidingParty, Zone.SHADOW_CHARACTERS);
 
         // End fellowship phase
