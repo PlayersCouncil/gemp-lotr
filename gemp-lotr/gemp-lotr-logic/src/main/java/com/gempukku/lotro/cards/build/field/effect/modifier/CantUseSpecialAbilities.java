96de2349d585416b888f9f225f0bc225d23fbc61
MarcinSc <marcins78@gmail.com>
2024-05-17 21:13:32 +0700

    Migrated Uruk-hai cards in set 11 to hjson

--- gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/cards/build/field/effect/modifier/CantUseSpecialAbilities.java
+++ gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/cards/build/field/effect/modifier/CantUseSpecialAbilities.java
@@ -1,9 +1,6 @@
 package com.gempukku.lotro.cards.build.field.effect.modifier;
 
-import com.gempukku.lotro.cards.build.CardGenerationEnvironment;
-import com.gempukku.lotro.cards.build.FilterableSource;
-import com.gempukku.lotro.cards.build.InvalidCardDefinitionException;
-import com.gempukku.lotro.cards.build.ModifierSource;
+import com.gempukku.lotro.cards.build.*;
 import com.gempukku.lotro.cards.build.field.FieldUtils;
 import com.gempukku.lotro.logic.modifiers.PlayersCantUseCardSpecialAbilitiesModifier;
 import org.json.simple.JSONObject;
@@ -11,11 +8,14 @@ import org.json.simple.JSONObject;
 public class CantUseSpecialAbilities implements ModifierSourceProducer {
     @Override
     public ModifierSource getModifierSource(JSONObject object, CardGenerationEnvironment environment) throws InvalidCardDefinitionException {
-        FieldUtils.validateAllowedFields(object, "filter");
+        FieldUtils.validateAllowedFields(object, "filter", "requires");
 
+        final JSONObject[] conditionArray = FieldUtils.getObjectArray(object.get("requires"), "requires");
         final String filter = FieldUtils.getString(object.get("filter"), "filter", "any");
+        final Requirement[] requirements = environment.getRequirementFactory().getRequirements(conditionArray, environment);
         final FilterableSource filterableSource = environment.getFilterFactory().generateFilter(filter, environment);
 
-        return actionContext -> new PlayersCantUseCardSpecialAbilitiesModifier(actionContext.getSource(), filterableSource.getFilterable(actionContext));
+        return actionContext -> new PlayersCantUseCardSpecialAbilitiesModifier(actionContext.getSource(),
+                new RequirementCondition(requirements, actionContext), filterableSource.getFilterable(actionContext));
     }
 }
