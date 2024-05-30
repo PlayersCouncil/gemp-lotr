1bb4e4dd19401e743fcd450cf0974a0aeaa94b58
Christian 'ketura' McCarty <hyperlink.teltura@gmail.com>
2024-05-29 21:16:06 -0500

    Altering ModifyRoamingPenalty to accept an evaluated value rather than an integer.

--- gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/logic/modifiers/RoamingPenaltyModifier.java
+++ gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/logic/modifiers/RoamingPenaltyModifier.java
@@ -3,17 +3,29 @@ package com.gempukku.lotro.logic.modifiers;
 import com.gempukku.lotro.common.Filterable;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
+import com.gempukku.lotro.logic.modifiers.evaluator.ConstantEvaluator;
+import com.gempukku.lotro.logic.modifiers.evaluator.Evaluator;
 
 public class RoamingPenaltyModifier extends AbstractModifier {
-    private final int _modifier;
+    private final Evaluator _evaluator;
 
     public RoamingPenaltyModifier(PhysicalCard source, Filterable affectFilter, Condition condition, int modifier) {
-        super(source, "Roaming penalty " + ((modifier > 0) ? ("+" + modifier) : modifier), affectFilter, condition, ModifierEffect.TWILIGHT_COST_MODIFIER);
-        _modifier = modifier;
+        this(source, affectFilter, condition, new ConstantEvaluator(modifier));
+    }
+
+    public RoamingPenaltyModifier(PhysicalCard source, Filterable affectFilter, Condition condition, Evaluator evaluator) {
+        super(source, null, affectFilter, condition, ModifierEffect.TWILIGHT_COST_MODIFIER);
+        _evaluator = evaluator;
+    }
+
+    @Override
+    public String getText(LotroGame game, PhysicalCard self) {
+        int modifier = _evaluator.evaluateExpression(game, self);
+        return "Roaming penalty " + ((modifier > 0) ? ("+" + modifier) : modifier);
     }
 
     @Override
     public int getRoamingPenaltyModifier(LotroGame game, PhysicalCard physicalCard) {
-        return _modifier;
+        return _evaluator.evaluateExpression(game, physicalCard);
     }
 }
