edc50eef1322579e7d40933ebab8f908c61bf7ee
Christian 'ketura' McCarty <hyperlink.teltura@gmail.com>
2024-09-06 21:00:41 -0500

    Enabling the playtest popup for every game on the playtest server.  Altering the playtesting tab to show all games.

--- gemp-lotr/gemp-lotr-server/src/main/java/com/gempukku/lotro/game/GameRecorder.java
+++ gemp-lotr/gemp-lotr-server/src/main/java/com/gempukku/lotro/game/GameRecorder.java
@@ -99,14 +99,14 @@ public class GameRecorder {
             var playerRecordingId = saveRecordedChannels(recordingChannels, gameInfo, decks);
             gameInfo.id = _gameHistoryService.addGameHistory(gameInfo);
 
-            if(format.isPlaytest())
-            {
+//            if(format.isPlaytest())
+//            {
                 String url = "https://docs.google.com/forms/d/e/1FAIpQLSdKJrCmjoyUqDTusDcpNoWAmvkGdzQqTxWGpdNIFX9biCee-A/viewform?usp=pp_url&entry.1592109986=";
                 String winnerURL = "https://play.lotrtcgpc.net/gemp-lotr/game.html%3FreplayId%3D" + winnerName + "$" + playerRecordingId.get(winnerName);
                 String loserURL = "https://play.lotrtcgpc.net/gemp-lotr/game.html%3FreplayId%3D" + loserName + "$" + playerRecordingId.get(loserName);
                 url += winnerURL + "%20" + loserURL;
-                lotroGame.sendMessageToPlayers("Thank you for playtesting!  If you have any feedback, bugs, or other issues to report about this match, <a href= '" + url + "'>please do so using this form.</a>");
-            }
+                lotroGame.sendMessageToPlayers("Thank you for playtesting!  Please report how you feel about this match (good, bad, or neutral) as well as any bugs or other issues <a href= '" + url + "'>using the playtest feedback form here.</a>");
+//            }
 
         };
     }
