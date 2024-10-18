f4ce01fc125de34046264b23326df939cc0b2339
Marcin Sciesinski <marcins78@gmail.com>
2024-10-18 13:32:04 +0700

    Fixed issue with cards creating assignments

--- gemp-lotr/gemp-lotr-server/src/test/java/com/gempukku/lotro/at/IndividualCardAtTest.java
+++ gemp-lotr/gemp-lotr-server/src/test/java/com/gempukku/lotro/at/IndividualCardAtTest.java
@@ -14,7 +14,10 @@ import org.junit.Test;
 
 import java.util.*;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 
 public class IndividualCardAtTest extends AbstractAtTest {
 
