9c065dc68900a76fc9e0516e06dd3b4042458078
Christian 'ketura' McCarty <hyperlink.teltura@gmail.com>
2024-11-20 21:08:03 -0600

    V2 errata and wording fixes
    - Updated wording for V2 Uruk Shield Wall and V2 An End Worthy of Remembrance
    - Fixed V1 Writhing Tentacle using an errata URL
    - Added errata for both Herugrim, Simbelmyne, Let Us Be Swift, No Rest for the Weary, and Sigewulf
    - Fixed a rare Neekerbreeker's Bog (d + e) issue where it would both heal and exert the same character if Lingering Shadow was in play.

---
+++ gemp-lotr/gemp-lotr-server/src/test/java/com/gempukku/lotro/cards/unofficial/pc/errata/set05/Card_05_092_ErrataTests.java
@@ -0,0 +1,80 @@
+package com.gempukku.lotro.cards.unofficial.pc.errata.set05;
+
+import com.gempukku.lotro.cards.GenericCardTestHelper;
+import com.gempukku.lotro.common.*;
+import com.gempukku.lotro.game.CardNotFoundException;
+import com.gempukku.lotro.game.PhysicalCardImpl;
+import com.gempukku.lotro.logic.decisions.DecisionResultInvalidException;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.*;
+
+public class Card_05_092_ErrataTests
+{
+
+	protected GenericCardTestHelper GetScenario() throws CardNotFoundException, DecisionResultInvalidException {
+		return new GenericCardTestHelper(
+				new HashMap<>()
+				{{
+					put("card", "55_92");
+					// put other cards in here as needed for the test case
+				}},
+				GenericCardTestHelper.FellowshipSites,
+				GenericCardTestHelper.FOTRFrodo,
+				GenericCardTestHelper.RulingRing
+		);
+	}
+
+	@Test
+	public void SigewulfStatsAndKeywordsAreCorrect() throws DecisionResultInvalidException, CardNotFoundException {
+
+		/**
+		 * Set: 5
+		 * Name: Sigewulf, Brave Volunteer
+		 * Unique: True
+		 * Side: Free Peoples
+		 * Culture: Rohan
+		 * Twilight Cost: 1
+		 * Type: Ally
+		 * Subtype: Man
+		 * Strength: 4
+		 * Vitality: 2
+		 * Site Number: 4T
+		 * Game Text: <b>Villager</b>.<br><b>Regroup:</b> Exert Sigewulf and spot 5 valiant companions to discard a minion.
+		*/
+
+		var scn = GetScenario();
+
+		var card = scn.GetFreepsCard("card");
+
+		assertEquals("Sigewulf", card.getBlueprint().getTitle());
+		assertEquals("Brave Volunteer", card.getBlueprint().getSubtitle());
+		assertTrue(card.getBlueprint().isUnique());
+		assertEquals(Side.FREE_PEOPLE, card.getBlueprint().getSide());
+		assertEquals(Culture.ROHAN, card.getBlueprint().getCulture());
+		assertEquals(CardType.ALLY, card.getBlueprint().getCardType());
+		assertEquals(Race.MAN, card.getBlueprint().getRace());
+		assertTrue(scn.hasKeyword(card, Keyword.VILLAGER));
+		assertEquals(1, card.getBlueprint().getTwilightCost());
+		assertEquals(4, card.getBlueprint().getStrength());
+		assertEquals(2, card.getBlueprint().getVitality());
+		assertTrue(card.getBlueprint().hasAllyHome(new AllyHome(SitesBlock.TWO_TOWERS, 4)));
+	}
+
+	// Uncomment any @Test markers below once this is ready to be used
+	//@Test
+	public void SigewulfTest1() throws DecisionResultInvalidException, CardNotFoundException {
+		//Pre-game setup
+		var scn = GetScenario();
+
+		var card = scn.GetFreepsCard("card");
+		scn.FreepsMoveCardToHand(card);
+
+		scn.StartGame();
+		scn.FreepsPlayCard(card);
+
+		assertEquals(1, scn.GetTwilight());
+	}
+}
