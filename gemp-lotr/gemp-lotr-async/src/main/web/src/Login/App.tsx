f93ed27f8350553d6830b5371e2e0d77ae62e567
Nathaniel Bond <nabond251@gmail.com>
2025-01-01 23:16:22 -0500

    Scale content back; match og

--- gemp-lotr/gemp-lotr-async/src/main/web/src/Login/App.tsx
+++ gemp-lotr/gemp-lotr-async/src/main/web/src/Login/App.tsx
@@ -1,26 +1,13 @@
 function App() {
   return (
     <>
-        <div className="dim"></div>
-        <div className="centerContainer">
-            <h1 className="banner">GEMP: Play the Lord of the Rings TCG</h1>
-            <div className="content">
-                <div className="login">
-                    <div className="status"></div>
-                    <div className="error"></div>
-                    <div className="interaction"></div>
-                </div>
-                <div className="info">
-                    <p><a href="https://wiki.lotrtcgpc.net/wiki/The_Lord_of_the_Rings_TCG">The Lord of the Rings TCG</a> was published by Decipher, Inc from 2001-2007.<br />GEMP is a platform for playing this excellent game for free in your browser.<br />Maintained by the unofficial <a href="https://lotrtcgpc.net/">Player's Council</a>.</p>
-                    
-                    <div className="sw-shoutout">
-                        <img src="https://res.starwarsccg.org/gemp/lightForce-42.png" height="20"/>
-                        <p>(Also check out the <a href="https://gemp.starwarsccg.org/gemp-swccg/">Star Wars CCG GEMP</a> ran by the <a href="https://www.starwarsccg.org/">Star Wars Players Committee</a>.)</p>
-                        <img src="https://res.starwarsccg.org/gemp/darkForce-42.png" height="20"/>
-                    </div>
-                </div>
-            </div>
-        </div>
+      <p><a href="https://wiki.lotrtcgpc.net/wiki/The_Lord_of_the_Rings_TCG">The Lord of the Rings TCG</a> was published by Decipher, Inc from 2001-2007.<br />GEMP is a platform for playing this excellent game for free in your browser.<br />Maintained by the unofficial <a href="https://lotrtcgpc.net/">Player's Council</a>.</p>
+        
+      <div className="sw-shoutout">
+        <img src="https://res.starwarsccg.org/gemp/lightForce-42.png" height="20"/>
+        <p>(Also check out the <a href="https://gemp.starwarsccg.org/gemp-swccg/">Star Wars CCG GEMP</a> ran by the <a href="https://www.starwarsccg.org/">Star Wars Players Committee</a>.)</p>
+        <img src="https://res.starwarsccg.org/gemp/darkForce-42.png" height="20"/>
+      </div>
     </>
   )
 }
