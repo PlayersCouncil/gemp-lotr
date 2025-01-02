beb988192d86a20aefc74d371566545d69cd70e8
Nathaniel Bond <nabond251@gmail.com>
2025-01-01 21:15:03 -0500

    Try bringing in initial react

--- gemp-lotr/gemp-lotr-client/vite.config.ts
+++ gemp-lotr/gemp-lotr-client/vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "../gemp-lotr-async/src/main/web/src/Login"
+  }
 })
