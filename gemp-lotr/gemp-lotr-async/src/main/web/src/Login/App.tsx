0462b53f0c66c9a1f7d2c1cd49175bfac04d62e3
Nathaniel Bond <nabond251@gmail.com>
2025-01-01 23:04:23 -0500

    Migrate content

--- gemp-lotr/gemp-lotr-async/src/main/web/src/Login/App.tsx
+++ gemp-lotr/gemp-lotr-async/src/main/web/src/Login/App.tsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <div className="dim"></div>
+        <div className="centerContainer">
+            <h1 className="banner">GEMP: Play the Lord of the Rings TCG</h1>
+            <div className="content">
+                <div className="login">
+                    <div className="status"></div>
+                    <div className="error"></div>
+                    <div className="interaction"></div>
+                </div>
+                <div className="info">
+                    <p><a href="https://wiki.lotrtcgpc.net/wiki/The_Lord_of_the_Rings_TCG">The Lord of the Rings TCG</a> was published by Decipher, Inc from 2001-2007.<br />GEMP is a platform for playing this excellent game for free in your browser.<br />Maintained by the unofficial <a href="https://lotrtcgpc.net/">Player's Council</a>.</p>
+                    
+                    <div className="sw-shoutout">
+                        <img src="https://res.starwarsccg.org/gemp/lightForce-42.png" height="20"/>
+                        <p>(Also check out the <a href="https://gemp.starwarsccg.org/gemp-swccg/">Star Wars CCG GEMP</a> ran by the <a href="https://www.starwarsccg.org/">Star Wars Players Committee</a>.)</p>
+                        <img src="https://res.starwarsccg.org/gemp/darkForce-42.png" height="20"/>
+                    </div>
+                </div>
+            </div>
+        </div>
     </>
   )
 }
