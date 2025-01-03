50e751d925b3f16645468929d188d5add8ff67d8
Nathaniel Bond <nabond251@gmail.com>
2025-01-02 20:39:32 -0500

    Set up initial interaction modes

--- gemp-lotr/gemp-lotr-async/src/main/web/src/Login/Login.tsx
+++ gemp-lotr/gemp-lotr-async/src/main/web/src/Login/Login.tsx
@@ -1,11 +1,49 @@
+import { useState } from "react"
 import "./Login.css"
 
 function Login() {
+  enum InteractionMode {
+    Welcome,
+    Registration,
+    Login,
+  }
+
+  const [mode, setMode] = useState(InteractionMode.Welcome)
+
   return (
     <>
       <div className="status"></div>
       <div className="error"></div>
-      <div className="interaction"></div>
+      <div className="interaction">
+        {
+          mode == InteractionMode.Welcome ?
+            <WelcomeInteraction /> :
+          mode == InteractionMode.Login ?
+            <LoginInteraction /> :
+            <RegistrationInteraction />
+        }
+      </div>
+    </>
+  )
+}
+
+function WelcomeInteraction() {
+  return (
+    <>
+    </>
+  )
+}
+
+function LoginInteraction() {
+  return (
+    <>
+    </>
+  )
+}
+
+function RegistrationInteraction() {
+  return (
+    <>
     </>
   )
 }
