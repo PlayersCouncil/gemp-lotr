e3d3ce684326a9d6d92de62ca1a85ecd5ee509bc
Christian 'ketura' McCarty <hyperlink.teltura@gmail.com>
2025-03-23 18:39:34 -0500

    Helpless errata, Pale Blade, Band of Wild Men unit tests

--- gemp-lotr/gemp-lotr-server/src/test/java/com/gempukku/lotro/cards/official/set01/Card_01_221_Tests.java
+++ gemp-lotr/gemp-lotr-server/src/test/java/com/gempukku/lotro/cards/official/set01/Card_01_221_Tests.java
@@ -3,7 +3,6 @@ package com.gempukku.lotro.cards.official.set01;
 import com.gempukku.lotro.cards.GenericCardTestHelper;
 import com.gempukku.lotro.common.*;
 import com.gempukku.lotro.game.CardNotFoundException;
-import com.gempukku.lotro.game.PhysicalCardImpl;
 import com.gempukku.lotro.logic.decisions.DecisionResultInvalidException;
 import org.junit.Test;
 
@@ -18,8 +17,14 @@ public class Card_01_221_Tests
 		return new GenericCardTestHelper(
 				new HashMap<>()
 				{{
-					put("card", "1_221");
-					// put other cards in here as needed for the test case
+					put("paleblade", "1_221");
+					put("twk", "1_237");
+
+					put("gimli", "1_13");
+					put("condition1", "1_16");
+					put("condition2", "1_21");
+					put("condition3", "2_1");
+					put("condition4", "2_9");
 				}},
 				GenericCardTestHelper.FellowshipSites,
 				GenericCardTestHelper.FOTRFrodo,
@@ -45,7 +50,7 @@ public class Card_01_221_Tests
 
 		var scn = GetScenario();
 
-		var card = scn.GetFreepsCard("card");
+		var card = scn.GetFreepsCard("paleblade");
 
 		assertEquals("The Pale Blade", card.getBlueprint().getTitle());
 		assertNull(card.getBlueprint().getSubtitle());
@@ -58,18 +63,56 @@ public class Card_01_221_Tests
 		assertEquals(3, card.getBlueprint().getStrength());
 	}
 
-	// Uncomment any @Test markers below once this is ready to be used
-	//@Test
-	public void ThePaleBladeTest1() throws DecisionResultInvalidException, CardNotFoundException {
+	@Test
+	public void ThePaleBladeCanBeTriggeredMultipleTimesFromSameVictory() throws DecisionResultInvalidException, CardNotFoundException {
 		//Pre-game setup
 		var scn = GetScenario();
 
-		var card = scn.GetFreepsCard("card");
-		scn.FreepsMoveCardToHand(card);
+		var twk = scn.GetShadowCard("twk");
+		var paleblade = scn.GetShadowCard("paleblade");
+		scn.ShadowMoveCharToTable(twk);
+		scn.AttachCardsTo(twk, paleblade);
+
+		var gimli = scn.GetFreepsCard("gimli");
+		var condition1 = scn.GetFreepsCard("condition1");
+		var condition2 = scn.GetFreepsCard("condition2");
+		var condition3 = scn.GetFreepsCard("condition3");
+		var condition4 = scn.GetFreepsCard("condition4");
+		scn.FreepsMoveCharToTable(gimli);
+		scn.FreepsMoveCardToSupportArea(condition1, condition2, condition3, condition4);
 
 		scn.StartGame();
-		scn.FreepsPlayCard(card);
+		scn.SkipToAssignments();
+		scn.FreepsAssignToMinions(gimli, twk);
+		scn.FreepsResolveSkirmish(gimli);
+		assertEquals(Zone.SUPPORT, condition1.getZone());
+		assertEquals(Zone.SUPPORT, condition2.getZone());
+		assertEquals(Zone.SUPPORT, condition3.getZone());
+		assertEquals(Zone.SUPPORT, condition4.getZone());
+		assertEquals(0, scn.GetWoundsOn(twk));
 
-		assertEquals(2, scn.GetTwilight());
+		scn.PassCurrentPhaseActions();
+
+		assertTrue(scn.ShadowHasOptionalTriggerAvailable());
+		scn.ShadowAcceptOptionalTrigger();
+		assertEquals(1, scn.GetWoundsOn(twk));
+		scn.ShadowChooseCard(condition1);
+		assertEquals(Zone.DISCARD, condition1.getZone());
+
+		assertTrue(scn.ShadowHasOptionalTriggerAvailable());
+		scn.ShadowAcceptOptionalTrigger();
+		assertEquals(2, scn.GetWoundsOn(twk));
+		scn.ShadowChooseCard(condition2);
+		assertEquals(Zone.DISCARD, condition2.getZone());
+
+		assertTrue(scn.ShadowHasOptionalTriggerAvailable());
+		scn.ShadowAcceptOptionalTrigger();
+		assertEquals(3, scn.GetWoundsOn(twk));
+		scn.ShadowChooseCard(condition3);
+		assertEquals(Zone.DISCARD, condition3.getZone());
+
+		assertFalse(scn.ShadowHasOptionalTriggerAvailable());
+		assertEquals(Phase.ASSIGNMENT, scn.GetCurrentPhase());
+
 	}
 }
