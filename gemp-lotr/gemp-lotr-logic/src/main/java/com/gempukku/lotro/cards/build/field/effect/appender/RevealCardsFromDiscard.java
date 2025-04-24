f3fa33658c4db8c9bc7b81c901f603b9e1b2d6c4
Christian 'ketura' McCarty <hyperlink.teltura@gmail.com>
2025-04-23 22:26:27 -0500

    Fixed cards using RevealCards not actually working, as that *only* works when revealing cards from play (rather useless)

---
+++ gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/cards/build/field/effect/appender/RevealCardsFromDiscard.java
@@ -0,0 +1,46 @@
+package com.gempukku.lotro.cards.build.field.effect.appender;
+
+import com.gempukku.lotro.cards.build.ActionContext;
+import com.gempukku.lotro.cards.build.CardGenerationEnvironment;
+import com.gempukku.lotro.cards.build.InvalidCardDefinitionException;
+import com.gempukku.lotro.cards.build.ValueSource;
+import com.gempukku.lotro.cards.build.field.FieldUtils;
+import com.gempukku.lotro.cards.build.field.effect.EffectAppender;
+import com.gempukku.lotro.cards.build.field.effect.EffectAppenderProducer;
+import com.gempukku.lotro.cards.build.field.effect.appender.resolver.CardResolver;
+import com.gempukku.lotro.cards.build.field.effect.appender.resolver.ValueResolver;
+import com.gempukku.lotro.game.PhysicalCard;
+import com.gempukku.lotro.logic.actions.CostToEffectAction;
+import com.gempukku.lotro.logic.effects.RevealCardEffect;
+import com.gempukku.lotro.logic.timing.Effect;
+import org.json.simple.JSONObject;
+
+import java.util.Collection;
+
+public class RevealCardsFromDiscard implements EffectAppenderProducer {
+    @Override
+    public EffectAppender createEffectAppender(boolean cost, JSONObject effectObject, CardGenerationEnvironment environment) throws InvalidCardDefinitionException {
+        FieldUtils.validateAllowedFields(effectObject, "count", "select", "memorize");
+
+        final String select = FieldUtils.getString(effectObject.get("select"), "select", "choose(any)");
+        final String memorize = FieldUtils.getString(effectObject.get("memorize"), "memorize", "_temp");
+
+        final ValueSource countSource = ValueResolver.resolveEvaluator(effectObject.get("count"), 1, environment);
+
+        MultiEffectAppender result = new MultiEffectAppender();
+
+        result.addEffectAppender(
+                CardResolver.resolveCardsInDiscard(select, countSource, memorize, "you", "Choose cards to reveal", environment));
+        result.addEffectAppender(
+                new DelayedAppender() {
+                    @Override
+                    protected Effect createEffect(boolean cost, CostToEffectAction action, ActionContext actionContext) {
+                        final Collection<? extends PhysicalCard> cardsToReveal = actionContext.getCardsFromMemory(memorize);
+                        return new RevealCardEffect(actionContext.getSource(), cardsToReveal);
+                    }
+                });
+
+        return result;
+    }
+
+}
