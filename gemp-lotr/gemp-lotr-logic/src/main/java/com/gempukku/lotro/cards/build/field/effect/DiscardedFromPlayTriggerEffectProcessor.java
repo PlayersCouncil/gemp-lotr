4a365f5c85461ea6bd6a54fb886d710e817e0f1a
Christian 'ketura' McCarty <hyperlink.teltura@gmail.com>
2025-06-26 23:51:23 -0500

    New V3 Pipeweed implementations
    - Added 'source' parameter to the DiscardedFromPlay top-level action

--- gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/cards/build/field/effect/DiscardedFromPlayTriggerEffectProcessor.java
+++ gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/cards/build/field/effect/DiscardedFromPlayTriggerEffectProcessor.java
@@ -1,21 +1,27 @@
 package com.gempukku.lotro.cards.build.field.effect;
 
-import com.gempukku.lotro.cards.build.BuiltLotroCardBlueprint;
-import com.gempukku.lotro.cards.build.CardGenerationEnvironment;
-import com.gempukku.lotro.cards.build.InvalidCardDefinitionException;
+import com.gempukku.lotro.cards.build.*;
 import com.gempukku.lotro.cards.build.field.EffectProcessor;
 import com.gempukku.lotro.cards.build.field.FieldUtils;
+import com.gempukku.lotro.filters.Filters;
 import org.json.simple.JSONObject;
 
 public class DiscardedFromPlayTriggerEffectProcessor implements EffectProcessor {
     @Override
     public void processEffect(JSONObject value, BuiltLotroCardBlueprint blueprint, CardGenerationEnvironment environment) throws InvalidCardDefinitionException {
-        FieldUtils.validateAllowedFields(value, "optional", "requires", "cost", "effect");
+        FieldUtils.validateAllowedFields(value, "optional", "source", "requires", "cost", "effect");
 
         final boolean optional = FieldUtils.getBoolean(value.get("optional"), "optional", false);
+        final String source = FieldUtils.getString(value.get("source"), "source", "any");
+
+        final FilterableSource bySource = source != null ? environment.getFilterFactory().generateFilter(source, environment) : null;
 
         DefaultActionSource triggerActionSource = new DefaultActionSource();
         EffectUtils.processRequirementsCostsAndEffects(value, environment, triggerActionSource);
+
+        triggerActionSource.addPlayRequirement(
+                actionContext -> Filters.accepts(actionContext.getGame(), actionContext.getSource(), bySource.getFilterable(actionContext)));
+
         if (optional)
             blueprint.setDiscardedFromPlayOptionalTriggerAction(triggerActionSource);
         else
