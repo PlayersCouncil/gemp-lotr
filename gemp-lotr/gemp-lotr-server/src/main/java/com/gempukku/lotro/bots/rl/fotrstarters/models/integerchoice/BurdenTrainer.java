bac0aee1c2cef4293724b4b26a822abfa189e5d6
jakub.salavec <jakub.salavec@target-technologie.cz>
2025-07-15 13:20:27 +0200

    Modified - burden bidding modified to random choice in 0-4 range since frodo is not guaranteed

--- gemp-lotr/gemp-lotr-server/src/main/java/com/gempukku/lotro/bots/rl/fotrstarters/models/integerchoice/BurdenTrainer.java
+++ gemp-lotr/gemp-lotr-server/src/main/java/com/gempukku/lotro/bots/rl/fotrstarters/models/integerchoice/BurdenTrainer.java
@@ -1,6 +1,13 @@
 package com.gempukku.lotro.bots.rl.fotrstarters.models.integerchoice;
 
 
+import com.gempukku.lotro.bots.rl.RLGameStateFeatures;
+import com.gempukku.lotro.bots.rl.fotrstarters.models.ModelRegistry;
+import com.gempukku.lotro.game.state.GameState;
+import com.gempukku.lotro.logic.decisions.AwaitingDecision;
+
+import java.util.Random;
+
 public class BurdenTrainer extends AbstractIntegerTrainer {
     private static final int MAX_BURDENS_TO_BID = 9;
 
@@ -11,5 +18,13 @@ public class BurdenTrainer extends AbstractIntegerTrainer {
 
     public int getMaxChoice() {
         return MAX_BURDENS_TO_BID;
+    }
+
+    @Override
+    public String getAnswer(GameState gameState, AwaitingDecision decision, String playerName, RLGameStateFeatures features, ModelRegistry modelRegistry) {
+        // Bid randomly 0-4 since now we cannot guarantee frodo as ring bearer
+        Random random = new Random();
+        int randomNumber = random.nextInt(5);
+        return String.valueOf(randomNumber);
     }
 }
