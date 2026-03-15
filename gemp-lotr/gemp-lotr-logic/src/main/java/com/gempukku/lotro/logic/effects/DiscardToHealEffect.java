f76b2c3dbe413a642565312aad9bc4271385a5d3
Christian 'ketura' McCarty <hyperlink.teltura@gmail.com>
2026-03-15 13:50:19 -0500

    Fixed Great Gate of MInas Tirith triggering on non-support special abilities

--- gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/logic/effects/DiscardToHealEffect.java
+++ gemp-lotr/gemp-lotr-logic/src/main/java/com/gempukku/lotro/logic/effects/DiscardToHealEffect.java
@@ -1,6 +1,7 @@
 package com.gempukku.lotro.logic.effects;
 
 import com.gempukku.lotro.common.Timeword;
+import com.gempukku.lotro.common.Zone;
 import com.gempukku.lotro.game.PhysicalCard;
 import com.gempukku.lotro.game.state.LotroGame;
 import com.gempukku.lotro.logic.GameUtils;
@@ -8,7 +9,7 @@ import com.gempukku.lotro.logic.timing.results.DiscardToHealResult;
 
 public class DiscardToHealEffect extends ActivateCardEffect {
     public DiscardToHealEffect(PhysicalCard source, String performingPlayer, Timeword actionTimeword) {
-        super(source, performingPlayer, actionTimeword);
+        super(source, performingPlayer, actionTimeword, Zone.HAND);
         _activateCardResult = new DiscardToHealResult(_source, performingPlayer, _actionTimeword);
     }
 
